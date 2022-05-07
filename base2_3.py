26fd7f2a22f5c0c07ee749f89944c57396bd3403
Gabe1712 <55352779+Gabe1712@users.noreply.github.com>
2022-05-07 11:00:14 +0200

    Add files via upload

---
+++ base2_3.py
@@ -0,0 +1,112 @@
+from os import remove
+from ssd.modeling import AnchorBoxes
+from tops.config import LazyCall as L
+from ssd.modeling.backbones.FPN import FPN
+
+##
+from ssd.data.transforms import (
+    ToTensor, RandomHorizontalFlip, RandomSampleCrop, Normalize, Resize,
+    GroundTruthBoxesToAnchors)
+import torch
+import torchvision
+import getpass
+import pathlib
+from configs.utils import get_dataset_dir, get_output_dir
+from ssd.data import MNISTDetectionDataset
+from torch.optim.lr_scheduler import MultiStepLR, LinearLR
+#from ssd.modeling import SSD300, SSDMultiboxLoss, backbones
+from ssd.modeling.FocalLoss import FocalLoss
+from ssd.data import TDT4265Dataset
+
+from ssd.modeling.retinanet import RetinaNet
+##
+
+
+# The line belows inherits the configuration set for the tdt4265 dataset
+from ..tdt4265 import (
+    train,
+    optimizer,
+    schedulers,
+    loss_objective,
+    model,
+    backbone,
+    data_train,
+    data_val,
+    train_cpu_transform,
+    val_cpu_transform,
+    gpu_transform,
+    label_map
+)
+
+# The config below is copied from the ssd300.py model trained on images of size 300*300.
+# The images in the tdt4265 dataset are of size 128 * 1024, so resizing to 300*300 is probably a bad idea
+# Change the imshape to (128, 1024) and experiment with better prior boxes
+train.imshape = (128, 1024)
+train.epochs = 20
+
+#Task 2.1
+anchors = L(AnchorBoxes)(
+    feature_sizes=[[32, 256], [16, 128], [8, 64], [4, 32], [2, 16], [1, 8]],
+    # Strides is the number of pixels (in image space) between each spatial position in the feature map
+    strides=[[4, 4], [8, 8], [16, 16], [32, 32], [64, 64], [128, 128]],
+    min_sizes=[[16, 16], [32, 32], [48, 48], [64, 64], [86, 86], [128, 128], [128, 400]],
+    # Strides is the number of pixels (in image space) between each spatial position in the feature map
+    # aspect ratio is defined per feature map (first index is largest feature map (38x38))
+    # aspect ratio is used to define two boxes per element in the list.
+    # if ratio=[2], boxes will be created with ratio 1:2 and 2:1
+    # Number of boxes per location is in total 2 + 2 per aspect ratio
+    aspect_ratios=[[2, 3], [2, 3], [2, 3], [2, 3], [2, 3], [2, 3]], #put the "aspect_ratios" for the anchor boxes the same at every feature map.
+    image_shape="${train.imshape}",
+    scale_center_variance=0.1,
+    scale_size_variance=0.2
+)
+
+#Task 2.2
+train_cpu_transform = L(torchvision.transforms.Compose)(transforms=[
+    L(RandomSampleCrop)(),
+    L(ToTensor)(),
+    L(RandomHorizontalFlip)(),
+    L(Resize)(imshape="${train.imshape}"),
+    L(GroundTruthBoxesToAnchors)(anchors="${anchors}", iou_threshold=0.5),
+])
+val_cpu_transform = L(torchvision.transforms.Compose)(transforms=[
+    L(ToTensor)(),
+    L(Resize)(imshape="${train.imshape}"),
+])
+gpu_transform = L(torchvision.transforms.Compose)(transforms=[
+    L(Normalize)(mean=[0.4765, 0.4774, 0.2259], std=[0.2951, 0.2864, 0.2878])
+])
+data_train.dataset = L(TDT4265Dataset)(
+    img_folder=get_dataset_dir("tdt4265_2022"),
+    transform="${train_cpu_transform}",
+    annotation_file=get_dataset_dir("tdt4265_2022/train_annotations.json"))
+data_val.dataset = L(TDT4265Dataset)(
+    img_folder=get_dataset_dir("tdt4265_2022"),
+    transform="${val_cpu_transform}",
+    annotation_file=get_dataset_dir("tdt4265_2022/val_annotations.json"))
+data_val.gpu_transform = gpu_transform
+data_train.gpu_transform = gpu_transform
+
+label_map = {idx: cls_name for idx, cls_name in enumerate(TDT4265Dataset.class_names)}
+
+
+#Task 2.3.1
+backbone = L(FPN)(
+    output_channels=[256, 256, 256, 256, 256, 256],
+    image_channels="${train.image_channels}",
+    output_feature_sizes="${anchors.feature_sizes}"
+)
+
+#Task 2.3.2
+loss_objective=L(FocalLoss)(anchors="${anchors}",alpha=[0.01,*[1 for i in range(model.num_classes-1)]],gamma=2)
+
+
+#Task 2.3.3 
+model = L(RetinaNet)(
+    feature_extractor="${backbone}",
+    anchors="${anchors}",
+    loss_objective="${loss_objective}",
+    num_classes=8 + 1 , # Add 1 for background
+    num_boxes =6,
+    num_channels=256
+)
